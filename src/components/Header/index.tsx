52befea532fc5118041760a2a256009bb05e0fff
Bruno-AlvesBR <brunoph.faces12@gmail.com>
2022-03-13 01:37:23 -0300

    feat: created component header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,29 @@
+import { Toolbar } from '@material-ui/core';
+
+import { UseCart } from '../../hooks/Cart';
+import router from "next/router";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+import { LinkCart, Navbar } from './style';
+
+export const Header = () => {
+  const { counter } = UseCart();
+
+  const handleGoToCart = () => {
+    router.push("/carrinho");
+  };
+
+  return (
+    <Navbar>
+      <Toolbar>
+        <LinkCart 
+          badgeContent={counter} 
+          onClick={handleGoToCart} 
+          color="primary"
+        >
+          <ShoppingCartIcon color="action" />
+        </LinkCart>
+      </Toolbar>
+    </Navbar>
+  );
+};
