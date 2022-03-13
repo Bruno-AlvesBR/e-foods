099b14851568b01d332832204b9a367145bb58cd
Bruno-AlvesBR <brunoph.faces12@gmail.com>
2022-03-13 01:34:58 -0300

    feat: created filters menu component

---
+++ src/components/Filters/style.ts
@@ -0,0 +1,24 @@
+import { styled } from '@material-ui/core';
+
+export const ContainerFilters = styled('div')(() => ({
+  display: 'flex',
+  flexDirection: 'column',
+  minWidth: 280,
+  height: 'fit-content',
+  background: '#fff',
+  boxShadow: '0 0 10px #00000020',
+  borderRadius: 10,
+  padding: 10,
+  gap: 5,
+
+  '& h1': {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+
+  '@media (max-width: 650px)': {
+    minWidth: 0,
+    width: '100%'
+  },
+}));
