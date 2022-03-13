d0d0f5cb614a761c38f7068deba2fe723f3075c2
Bruno-AlvesBR <brunoph.faces12@gmail.com>
2022-03-13 18:23:34 -0300

    feat: created logic to add or remove amount of item on cart

---
+++ src/components/CardCart/counterItem/index.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { UseCart } from '../../../hooks/Cart';
+
+import {
+  Container, 
+  ButtonAdd,
+  ButtonRemove,
+  InputAmount,
+} from './style';
+
+interface IProductProps {
+  productId: string;
+};
+
+export const ItemAmount = ({ productId }: IProductProps) => {
+  const [counterItem, setCounterItem] = useState(1);
+  const { handleRemoveProductCart } = UseCart();
+
+  const handleAddAmount = () => {
+    setCounterItem(counterItem + 1)
+  };
+
+  const handleRemoveAmount = () => {
+    if(counterItem <= 0) {
+      handleRemoveProductCart(productId);
+    };
+    setCounterItem(counterItem - 1)
+  };
+
+  return (
+    <Container>
+      <ButtonRemove 
+        onClick={handleRemoveAmount}
+      >
+        -
+      </ButtonRemove>
+      <InputAmount 
+        variant="standard" 
+        value={counterItem} 
+      />
+      <ButtonAdd
+        onClick={handleAddAmount}
+      >
+        +
+      </ButtonAdd>
+    </Container>
+  );
+};
